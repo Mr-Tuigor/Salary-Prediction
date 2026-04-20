54ce96222fe9b13eab258f95cc55c519a0b90a62
Shaharyar Ansari <shaharyaransarircb1@gmail.com>
2026-04-20 12:57:03 +0530

    Update job_titles.py

--- job_titles.py
+++ job_titles.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def get_job_titles():
     # Reload df2 to get original job titles before one-hot encoding
-    df_original = pd.read_csv('/content/Salary_Data.csv')
+    df_original = pd.read_csv('Salary_Data.csv')
     # Clean and impute Job Title if necessary, as done in the notebook
     df_original['Job Title'] = df_original['Job Title'].fillna(df_original['Job Title'].mode()[0])
     unique_job_titles = sorted(df_original['Job Title'].unique().tolist())
